69bb9eccb053e8fe0076da7e0e698e8df933e4a3
Jeafreezy <jolaiyaemmanuel@gmail.com>
2020-07-24 08:16:00 +0100

    Read single and many lasio files

--- petroeval/__init__.py
+++ petroeval/__init__.py
@@ -1,4 +1,10 @@
+__all__=['read_lasio','read_lasios','plots','visualizations','evaluate_reservoir']
+
 from petroeval import visualizations
 from petroeval import evaluate_reservoir
 from .evaluate_reservoir import FormationEvaluation
 from .visualizations import log_plot
+from petroeval import files_reader
+from petroeval.files_reader import  read_lasio,read_lasios
+
+from .plots import *
---
+++ petroeval/files_reader.py
@@ -0,0 +1,49 @@
+import lasio
+
+def read_lasio(path:'str')->'object':
+    """
+    This function reads well logs in las files
+    args:
+    path(str) :paths to lasio file
+
+    returns: las objects
+
+    """
+    assert isinstance(path, str), 'File must be in a string'
+    assert path.endswith('LAS'), 'File must be a valid Lasio files'
+
+    try:
+
+        return lasio.read(path)
+
+    except FileNotFoundError as err :
+
+        raise err
+
+
+def read_lasios(*args)->'list':
+
+    """
+    This function reads well logs in las files
+    args:
+    path(str) :paths to lasio file
+
+    returns: a list of las objects
+
+    """
+
+    assert [path.endswith('LAS') for path in args] ,'Files must be a valid lasio files'
+
+    try:
+            files=list()
+
+            for _ in args:
+
+                files.append(lasio.read(_))
+
+            return files
+
+    except Exception as err:
+
+        raise err
+
