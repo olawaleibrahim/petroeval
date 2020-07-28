ef527968d36b19eaee96f5ff4129786f590cbd71
olawaleibrahim <ibrahim.olawale13@gmail.com>
2020-07-28 11:30:32 +0100

    Preprocessing module

---
+++ petroeval/preprocessing.py
@@ -0,0 +1,11 @@
+'''
+Module for preprocessing las file before formation evaluation
+'''
+
+def set_mnemonics(data):
+    
+    '''
+    Function to set logs mnemonics to a standard title for later use
+    args::
+            data: dataframe object of well logs
+    '''
